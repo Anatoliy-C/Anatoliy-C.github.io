1f269bc4021e7580c55692a62c29a18acac459aa
Anatoliy <atsarkov.13@gmail.com>
2020-04-02 15:52:19 +0300

    V1.5

---
+++ DailyPass/js/app.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+
+
+
+    $('[data-modal="consultation"]').on('click', function () {
+        $('.overley-form, #consultation').fadeIn('slow');
+    });
+
+    $('.modal_close').on('click', function () {
+        $('.overley-form, #consultation').fadeOut('slow');
+    });
+
+    //E-mail Ajax Send
+    // $("form").submit(function () { //Change
+    //     var th = $(this);
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "mail.php", //Change
+    //         data: th.serialize()
+    //     }).done(function () {
+    //         alert("Thank you!");
+    //         setTimeout(function () {
+    //             // Done Functions
+    //             th.trigger("reset");
+    //         }, 1000);
+    //     });
+    //     return false;
+    // });
+
+
+
+
+});
